d47364d6e1207d71e3106615fd2c5e18c5d04e80
Pandiyan K <kpandiyan306@gmail.com>
2022-03-13 08:43:16 -0700

    Add file

---
+++ short.js
@@ -0,0 +1,17 @@
+function playAudio(id){
+    let animal = document.getElementById(id);
+animal.play()
+}
+// playAudio('lion');
+// playAudio('tiger');
+
+
+// function playAudio(id){
+//     document.getElementById(id);
+//     let animal =id;
+    
+// animal.play()
+// }
+// playAudio(lion);
+// playAudio('tiger');
+
